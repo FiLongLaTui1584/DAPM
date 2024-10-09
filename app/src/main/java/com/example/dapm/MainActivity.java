20e36c82404f418d01da54896b47df860a3dba77
Phi Long <philongt474@gmail.com>
2024-10-09 22:19:44 +0700

    Initial commit

---
+++ app/src/main/java/com/example/dapm/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.dapm;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.os.Bundle;
+import com.example.dapm.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setBackground(null);
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            int itemId = item.getItemId();
+
+            if (itemId == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (itemId == R.id.tindang) {
+                replaceFragment(new TinDangFragment());
+            } else if (itemId == R.id.giohang) {
+                replaceFragment(new CartFragment());
+            } else if (itemId == R.id.taikhoan) {
+                replaceFragment(new AccountFragment());
+            }
+
+            return true;
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
+    }
+}
