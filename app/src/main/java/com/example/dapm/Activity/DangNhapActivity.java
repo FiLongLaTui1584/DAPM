e4147b4cd099526b319c510d49a4634ec04f1fb5
Phi Long <philongt474@gmail.com>
2024-10-15 19:48:01 +0700

    DNhap

--- app/src/main/java/com/example/dapm/Activity/DangNhapActivity.java
+++ app/src/main/java/com/example/dapm/Activity/DangNhapActivity.java
@@ -10,12 +10,12 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.example.dapm.R;
 
-public class DangKyActivity extends AppCompatActivity {
+public class DangNhapActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_dang_ky);
+        setContentView(R.layout.activity_dang_nhap);
     }
 }
